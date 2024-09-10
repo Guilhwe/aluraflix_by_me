a005a9085ab02c762ce61264798dbd4b64129348
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-10 22:32:19 +0200

    creacion de una vista en admin para agregar videos en el banco de datos.

--- aluraflix/admin.py
+++ aluraflix/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from aluraflix.models import Video
 
-# Register your models here.
+class Videos(admin.ModelAdmin):
+    list_display = ('id','titulo','description','url',)
+    list_display_links =('id','titulo',)
+    list_per_page = 5
+    search_fields = ('id','titulo')
+
+admin.site.register(Video,Videos)
--- aluraflix/models.py
+++ aluraflix/models.py
@@ -4,3 +4,6 @@ class Video(models.Model):
     titulo = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(max_length=300, null=False, blank=False)
     url = models.URLField(max_length = 50, null=False, blank=False )
+
+    def __str__(self):
+        return self.titulo
