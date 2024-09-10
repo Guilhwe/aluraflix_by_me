d5018812fd0444dfc6c1e607bbf6a8a57e397973
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-10 23:12:09 +0200

    creacion de la url correcta utilizando routers para evitar pasar demasiados paths en la urlpatterns a modo de prevencion y buenaspractcas por si mas adelante se añadieran nuevas rutas

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from aluraflix.views import videos
+from aluraflix.views import VideoViewSet,VideoSerializer
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('videos',VideoViewSet,basename='videos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('videos/',videos)
+    path('',include(router.urls))
 ]
