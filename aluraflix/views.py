0158077e968bd28c4e1448e20a1d4c1bdae3f9fa
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-10 23:05:07 +0200

    modificacion de las views para que accedan a la base de dats yu no al video de ejemplo inicial a falta de configurar las urls

--- aluraflix/views.py
+++ aluraflix/views.py
@@ -1,11 +1,7 @@
-from django.http import JsonResponse
+from aluraflix.models import Video
+from aluraflix.serializers import VideoSerializer
+from rest_framework import viewsets
 
-def videos(request):
-    if request.method == 'GET':
-        video = {
-            'id':'1',
-            'titulo':'O que faz uma desenvolvedora front-end?',
-            'url':'https://www.youtube.com/watch?v=ZY3-MFxVdEw',
-            'decription':'O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!'
-        }   
-        return JsonResponse(video)
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
