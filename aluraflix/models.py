545e26d1ae5682ab538f3a8d9c14dc7cc9ea2d53
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-10 21:55:55 +0200

    creacion de superuser en admin y modificacion de models con su respectiva migracion

--- aluraflix/models.py
+++ aluraflix/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Video(models.Model):
     titulo = models.CharField(max_length=100, null=False, blank=False)
-    descripcion = models.TextField(max_length=300, null=False, blank=False)
+    description = models.TextField(max_length=300, null=False, blank=False)
     url = models.URLField(max_length = 50, null=False, blank=False )
