bded768cd6ae5948d4ec9263cf66d75c9440c02c
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-11 21:36:06 +0200

    añadido de un filtro para facilitar la localizacion de los videos por su id aunque non redirige el a su pagina. para accesar a la pagina se ha de poner en el buscador el endpoint

--- aluraflix/views.py
+++ aluraflix/views.py
@@ -1,7 +1,11 @@
 from aluraflix.models import Video
 from aluraflix.serializers import VideoSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets,filters,generics
+from django_filters.rest_framework import DjangoFilterBackend
 
 class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+    filter_backends =[DjangoFilterBackend, filters.SearchFilter]
+    search_fields =['id']
+   
--- setup/settings.py
+++ setup/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'aluraflix',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
