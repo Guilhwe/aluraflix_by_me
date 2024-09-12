505705d71ac7e4ce9448079d643398e8c3223328
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-12 18:52:18 +0200

    Same

--- aluraflix/models.py
+++ aluraflix/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+class Categoria:
+    nombre = models.CharField(max_length=15, null=False, blank=False)
+    color = models.CharField(max_length=10,null=False,blank=False)
+    def __str__(self):
+        return self.nombre
+
 class Video(models.Model):
+
+
     titulo = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(max_length=300, null=False, blank=False)
     url = models.URLField(max_length = 50, null=False, blank=False )
 
     def __str__(self):
         return self.titulo
+    
