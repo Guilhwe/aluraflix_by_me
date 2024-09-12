bc23b47c37e6ef4efe475f05b5f557fe45fed6d3
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-12 22:16:18 +0200

    redo

--- aluraflix/views.py
+++ aluraflix/views.py
@@ -12,4 +12,6 @@ class VideoViewSet(viewsets.ModelViewSet):
 class Categoria1ViewSet(viewsets.ModelViewSet):
     queryset=Categoria1.objects.all()
     serializer_class=Categoria1Serializer
+    filter_backends =[DjangoFilterBackend, filters.SearchFilter]
+    search_fields =['id']
    
