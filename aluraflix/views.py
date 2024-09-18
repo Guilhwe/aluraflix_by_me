550718a4b9408e4a8c722a7524166581eb41a38b
Guilherme Melo <guilherme96melo@gmail.com>
2024-09-18 20:23:17 +0200

    intentos de autenticacion. funciona la autenticacion pero no se consiguie mostrar un mensaje personalizado al fallar la antenticacion

--- aluraflix/views.py
+++ aluraflix/views.py
@@ -2,20 +2,38 @@ from aluraflix.models import Video,Categoria1
 from aluraflix.serializers import VideoSerializer,Categoria1Serializer, ListaVideosPorCategoriaSerializer
 from rest_framework import viewsets,filters,generics
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework .exceptions import AuthenticationFailed
+from rest_framework.response import Response
+
+class CustomBasicAuthentication(BasicAuthentication):
+    def authenticate(self, request):
+        user_auth = super().authenticate(request)
+        if not user_auth:
+            raise AuthenticationFailed(detail="Credenciales no válidas. Por favor, verifique su usuario y contraseña.")
+        return user_auth
+    
 
 class VideoViewSet(viewsets.ModelViewSet):
+    authentication_classes = [CustomBasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
     filter_backends =[DjangoFilterBackend, filters.SearchFilter]
     search_fields =['id']
 
 class Categoria1ViewSet(viewsets.ModelViewSet):
+    authentication_classes = [CustomBasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset=Categoria1.objects.all()
     serializer_class=Categoria1Serializer
     filter_backends =[DjangoFilterBackend, filters.SearchFilter]
     search_fields =['id']
 
 class ListaVideoPorCategoria(generics.ListAPIView):
+    authentication_classes = [CustomBasicAuthentication]
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         queryset = Video.objects.filter(categoria = self.kwargs['pk'])
         return queryset
